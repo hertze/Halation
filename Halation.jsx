32e3bca39791d4d22250c858af639e304a6a87c4
Joakim Hertze <joakim@hertze.com>
2024-02-19 12:46:18 +0100

    Update Halation.jsx

--- Halation.jsx
+++ Halation.jsx
@@ -40,6 +40,130 @@ var effect_opacity = 100;
 // END__HARVEST_EXCEPTION_ZSTRING
 */
 
+
+function displayDialog(thisRecipe, saveStatus, runmode) {
+	// Display dialog box.
+	var dialog = new Window("dialog");
+	dialog.text = "Halation";
+	dialog.orientation = "column";
+	dialog.alignChildren = ["left", "top"];
+	dialog.spacing = 10;
+	dialog.margins = 20;
+
+	dialog.statictext1 = dialog.add("statictext", undefined, undefined, { name: "label" });
+	if (runmode != "edit") {
+		dialog.statictext1.text = "Paste your recipe here:";
+	} else {
+		dialog.statictext1.text = "Edit your recipe here:";
+	}
+	dialog.statictext1.alignment = ["fill", "top"];
+
+	dialog.edittext1 = dialog.add("edittext", undefined, undefined, { multiline: true });
+	dialog.edittext1.alignment = ["fill", "top"];
+	dialog.edittext1.size = [500, 50];
+	dialog.edittext1.text = thisRecipe ? thisRecipe : '';
+	
+	dialog.savestatus = dialog.add("checkbox", undefined, "Save and close when done");
+	if (saveStatus !== undefined) {
+		dialog.savestatus.value = (saveStatus.toLowerCase() === "true");
+	} else {
+		dialog.savestatus.value
+	}
+
+	var buttons = dialog.add( "group" );
+	var submit = buttons.add("button", undefined, undefined, { name: "submit" });
+	submit.text = "Use this recipe";
+	
+	submit.onClick = function () {
+		thisRecipe = dialog.edittext1.text;
+		saveStatus = dialog.savestatus.value.toString();
+		dialog.close();
+	};
+	
+	if (runmode != "edit") {
+		var without = buttons.add("button", undefined, undefined, { name: "without" });
+		without.text = "Use default settings";
+		
+		without.onClick = function () {
+			thisRecipe = "none";
+			saveStatus = false;
+			dialog.close();
+		};
+	}
+	
+	dialog.show();
+
+	return {
+		"recipe": thisRecipe,
+		"savestatus": saveStatus
+	};
+}
+
+function getRecipe() {
+	// Retrieve recipe from action or dialog
+	if (!app.playbackParameters.count) {
+		//normal run (from scripts menu)
+
+		var result = displayDialog();
+		
+		if (!result.recipe || result.recipe == '') { isCancelled = true; return } else {
+			var d = new ActionDescriptor;
+			d.putString(stringIDToTypeID('recipe'), result.recipe);
+			d.putString(stringIDToTypeID('savestatus'), result.savestatus);
+			app.playbackParameters = d;		
+			return result;
+		}
+	}
+	else {
+		var recipe = app.playbackParameters.getString(stringIDToTypeID('recipe'));
+		var savestatus = app.playbackParameters.getString(stringIDToTypeID('savestatus'));
+		
+		if (app.playbackDisplayDialogs == DialogModes.ALL) {
+			// user run action in dialog mode (edit action step)
+			var result = displayDialog(recipe, savestatus, "edit");
+			if (!result.recipe || result.recipe == "") { isCancelled = true; return } else {
+				var d = new ActionDescriptor;
+				d.putString(stringIDToTypeID('recipe'), result.recipe);
+				d.putString(stringIDToTypeID('savestatus'), result.savestatus);
+				app.playbackParameters = d;
+			}
+			executeScript = false;
+			return result;
+		}
+		if (app.playbackDisplayDialogs != DialogModes.ALL) {
+			// user run script without recording
+			return {
+				"recipe": recipe,
+				"savestatus": savestatus
+			};
+		}
+	}
+}
+
+function processRecipe(runtimesettings) {
+	// Process the recipe and change settings
+	var thisRecipe = runtimesettings.recipe;
+	var saveStatus = runtimesettings.savestatus;
+	save = (saveStatus.toLowerCase() === "true");
+	thisRecipe = thisRecipe.replace(/\s+/g, ""); // Removes spaces
+	thisRecipe = thisRecipe.replace(/;+$/, ""); // Removes trailing ;
+	
+	// Check recipe against syntax
+	const regex = new RegExp('^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]);([0-9]|([1-9][0-9])|100);([0-9]|([1-9][0-9])|100)$', 'gm');
+	
+	if (regex.exec(thisRecipe) !== null) {
+		thisRecipe = thisRecipe.split(";"); // Splits into array at ;
+		threshold = parseInt(thisRecipe[0]);
+		blur_radius = parseInt(thisRecipe[1]);
+		effect_opacity = parseInt(thisRecipe[2]);
+
+	} else {
+		executeScript = false;
+		alert("Sorry, but that recipe is faulty! Please check it's syntax and it's settings and then try again.");
+	}
+}
+
+
 function colorOverlay() {
 	
 	var idset = stringIDToTypeID( "set" );
@@ -171,6 +295,9 @@ imagelayer.name = "original"; // Names background layer
 //
 
 try {
+	
+	var runtimesettings = getRecipe();
+	if (runtimesettings.recipe != "none") { processRecipe(runtimesettings); }
 	
 	var halationlayer = imagelayer.duplicate();
 	halationlayer.name = "halation"; // Names halation layer.
