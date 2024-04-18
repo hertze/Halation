d136f0236078e3f53c382068586373dbf4e6ac9a
Joakim Hertze <joakim@hertze.com>
2024-04-18 19:20:04 +0200

    Bug.

--- Halation.jsx
+++ Halation.jsx
@@ -261,6 +261,8 @@ function selectLowContrastAreas(imagelayer, threshold) {
 
 	var fuzz = doc_scale * Math.min(bloom, 20);
 
+	
+
 	lowContrastLayer.applyGaussianBlur(fuzz/2);
 
     // Create an alpha channel from the low contrast layer
@@ -277,7 +279,7 @@ function selectLowContrastAreas(imagelayer, threshold) {
     doc.selection.load(alphaChannel);
 
 	try {
-		doc.selection.contract(UnitValue(Math.round(doc_scale * Math.min(bloom, 20)), "px")); // Contract the selection
+		doc.selection.contract(UnitValue(Math.round(fuzz), "px")); // Contract the selection
 	} catch (e) {
 		// An error will be thrown if there is no selection, so you can ignore it
 	}
